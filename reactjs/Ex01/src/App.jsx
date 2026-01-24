7e5cc3cc4cee15c3c12b402fcf5b7220a6d922ac
Ly Thi Thuy <23711991.thuy@student.iuh.edu.vn>
2026-01-24 15:01:19 +0700

    part01_reactjs_bai01

---
+++ reactjs/Ex01/src/App.jsx
@@ -0,0 +1,29 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import ProductCard from './components/ProductCard'
+
+function App() {
+  const products = [
+    {
+      id: 1,
+      name: "Product 1",
+      price: 200000,
+      image: ""
+    },
+    {
+      id: 2,
+      name: "Product 2",
+      price: 300000,
+      image: ""
+    },
+  ];
+  return (
+    <div className="App">
+    
+    </div>
+  )
+}
+
+export default App
